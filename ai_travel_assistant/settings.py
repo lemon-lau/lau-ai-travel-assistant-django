d73f71692ed3a23336d9b8fe035856ffcb971331
Laura Migallen <lau@bposeats.com>
2025-06-16 14:00:22 +0800

    [Revision Commit] Add frontend deployed render url

--- ai_travel_assistant/settings.py
+++ ai_travel_assistant/settings.py
@@ -46,6 +46,7 @@ ALLOWED_HOSTS = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://ai-travel-assistant-u0rg.onrender.com"
 ]
 
 INSTALLED_APPS = [
@@ -60,6 +61,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', # FOR FE COMMUNICATION
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -67,8 +69,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # FOR FE COMMUNICATION
-    'corsheaders.middleware.CorsMiddleware'
+
 ]
 
 ROOT_URLCONF = 'ai_travel_assistant.urls'
